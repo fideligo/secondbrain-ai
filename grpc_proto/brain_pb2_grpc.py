9058ff7b7b0dcfe59901d7ae1a5708c710c6b2fa
Fideligo <mrafighalibf@gmail.com>
2026-06-12 01:26:34 +0700

    feat(proto): sync brain.proto with chat history structure

--- grpc_proto/brain_pb2_grpc.py
+++ grpc_proto/brain_pb2_grpc.py
@@ -50,8 +50,7 @@ class BrainServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def ProcessDocument(self, request, context):
-        """Go akan memanggil fungsi ini, mengirim dokumen, lalu Python membalas statusnya
-        """
+        """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
